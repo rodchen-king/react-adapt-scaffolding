7bcdce7bd851381130968a3df83ca7c752206396
rodchen <rodchen_king@outlook.com>
2022-01-04 14:54:09 +0800

    1. 提取css文件为单独的css文件

--- config/prd.js
+++ config/prd.js
@@ -2,13 +2,31 @@
  * @Description: 
  * @Author: rodchen
  * @Date: 2022-01-04 14:32:16
- * @LastEditTime: 2022-01-04 14:39:22
+ * @LastEditTime: 2022-01-04 14:52:41
  * @LastEditors: rodchen
  */
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { resolve } = require('path');
 const { merge } = require('webpack-merge');
 const commonConfig = require('./common')
 
 module.exports = merge(commonConfig, {
-  mode: 'production'
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.less$/,  // 匹配哪些文件
+        use: [ // use数组中的loader顺序是从右到左，从下到上的执行顺序
+          MiniCssExtractPlugin.loader,
+          'css-loader',  // 将css文件变成commonjs模块加载js中，里面内容是样式字符串，=
+          'less-loader'
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/built.css'
+    })
+  ]
 })
