84064f140dc2459819dcc4c206a9393785298593
rodchen <rodchen_king@outlook.com>
2022-01-04 11:46:38 +0800

    1. 解析loader css文件

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,10 @@
  * @Description: 
  * @Author: rodchen
  * @Date: 2022-01-04 11:08:33
- * @LastEditTime: 2022-01-04 11:15:03
+ * @LastEditTime: 2022-01-04 11:17:35
  * @LastEditors: rodchen
  */
-// import '../css/a.css';
-// import '../css/b.css';
+import '../css/a.css';
+import '../css/b.css';
 
 console.log(123)
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
  * @Description: 
  * @Author: rodchen
  * @Date: 2021-11-25 21:58:10
- * @LastEditTime: 2022-01-04 11:13:45
+ * @LastEditTime: 2022-01-04 11:38:52
  * @LastEditors: rodchen
  */
 
@@ -17,7 +17,13 @@ module.exports = {
   // loader配置
   module: {
     rules: [
-      
+      {
+        test: /\.css$/,  // 匹配哪些文件
+        use: [ // use数组中的loader顺序是从右到左，从下到上的执行顺序
+          'style-loader', // 将css-laoder生成的js文件， 创建styles标签，将js的css央视资源插入，添加到head中生效
+          'css-loader',  // 将css文件变成commonjs模块加载js中，里面内容是样式字符串，=
+        ]
+      }
     ]
   },
   // plugins
