8d6dd940af4dbf45fa8660690022639a7d1414eb
rodchen <rodchen_king@outlook.com>
2022-01-04 17:58:27 +0800

    1. 测试eslint

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,8 @@
  * @Description:
  * @Author: rodchen
  * @Date: 2022-01-04 11:08:33
- * @LastEditTime: 2022-01-04 17:55:21
+ * @LastEditTime: 2022-01-04 17:58:19
  * @LastEditors: rodchen
  */
 import '../css/a.less';
 import '../css/b.less';
-
-s;
