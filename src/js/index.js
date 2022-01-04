94f36c89204640e67d9fb0f3b269c8e6f713cd3b
rodchen <rodchen_king@outlook.com>
2022-01-04 18:00:28 +0800

    1. 添加eslint完成

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,14 @@
  * @Description:
  * @Author: rodchen
  * @Date: 2022-01-04 11:08:33
- * @LastEditTime: 2022-01-04 17:58:19
+ * @LastEditTime: 2022-01-04 18:00:08
  * @LastEditors: rodchen
  */
 import '../css/a.less';
 import '../css/b.less';
+
+function index() {
+
+}
+
+export default index;
